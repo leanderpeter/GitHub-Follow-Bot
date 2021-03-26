8be2492ca5a3d469220ff37ad6eba9ac4c972b14
Leander <kontakt@leanderpeter.de>
2021-03-26 15:55:14 +0100

    😈😈😈 added a unfollow script - works fine

--- unfollow_bot.py
+++ unfollow_bot.py
@@ -22,33 +22,24 @@ password.send_keys("password")
 login_form = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@value='Sign in']")))
 login_form.click()
 
-# Go to the followers tab
-nameOfUser = "substack" # Input github username here
+#own username
+nameOfUser = "username" # Input github username here
 
-driver.get("https://github.com/{}?tab=followers".format(nameOfUser))
-time.sleep(3)
+# iterate through all pages 
+for page in range(1, 50):
 
-# Find the followers
-users = driver.find_elements_by_xpath("//a[@data-hovercard-type='user']")
-temp = []
-for follower in users:
-    temp.append(follower.get_attribute("href"))
-list_set = set(temp) 
-followers = (list(list_set))
+	#get following users list (page)
+	driver.get("https://github.com/{}?page={}&tab=following".format(nameOfUser, page))
+	time.sleep(3)
 
-# Follow everyone who is following 'nameOfUser'
-for user in followers:
-    for page in range(1, 5):
-        string = "{}?page={}&tab=following".format(user, page)
-        driver.get(string)
-        time.sleep(3)
+	# unfollow all users
+	follow_button = driver.find_elements_by_xpath("//input[@aria-label='Unfollow this person']")
+	try:
+	    for i in follow_button:
+	        i.submit()
+	except:
+	    pass
 
-        follow_button = driver.find_elements_by_xpath("//input[@aria-label='Follow this person']")
 
-        try:
-            for i in follow_button:
-                i.submit()
-        except:
-            pass
 
-driver.close()
+
